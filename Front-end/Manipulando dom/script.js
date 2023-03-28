ac76f5ae3b4df07f159a3cf0a57c8faee1a89a03
Karen Alfaro <ksalfarom@gmail.com>
2023-03-28 22:00:52 +0200

    clase4 completada

--- Front-end/Manipulando dom/script.js
+++ Front-end/Manipulando dom/script.js
@@ -1,6 +1,7 @@
-const btn = document.querySelector('[data-form-btn]'); /*selecciona elemento dentro de nuestro arbol en este caso el botón agregar-- en index.html coloco data-form-btn en etiqueta button */ //1
+(() =>  { 
+  const btn = document.querySelector('[data-form-btn]'); /*selecciona elemento dentro de nuestro arbol en este caso el botón agregar-- en index.html coloco data-form-btn en etiqueta button */ //1
 //const input  = document.querySelector("[data-form-input]")     //5
-console.log(btn);   
+  
 //2
 //console.log(input.value);  //6 
 
@@ -18,15 +19,21 @@ const createTask = (evento) => {
    task.classList.add("card"); // agrego la clase de la lista
     input.value = ''; //limpieza input  // 2da parte clase 2
     // 3ra parte clase 2 console.log(value);  // 2da parte clase 2
-    const content = `<div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
+    const taskContent = document.createElement("div"); // esta línea reemplaza el <di> de la línea 23
+    taskContent.appendChild(checkComplete()); // el appendChild tiene un orden 1ro agregar los checkComplete, luego los titleTask
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(titleTask);
+    const content = `
+    
     </div>`; // backticks pego dentro de esas backticks el contenido del html del div q esta entre etiquetas comentariadas, se quita el hacer curso de DOM y se pone ---> $ {value}
-    task.innerHTML = content; // 3ra parte clase 2, con esta propiedad innerHTML se le dice q guarde lo ingresado por el usuario
+    // 5ta parte clase 4 task.innerHTML = content; // 3ra parte clase 2, con esta propiedad innerHTML se le dice q guarde lo ingresado por el usuario
+    task.appendChild(taskContent);
 
     list.appendChild(task); // 4ta parte clase 2, agregar a la lista
  
-    console.log(content); // 2da parte clase 2
+    //console.log(content); // 2da parte clase 2
     //6 
 };
 
@@ -34,4 +41,21 @@ const createTask = (evento) => {
 btn.addEventListener('click', createTask);
 // listener es el click de arriba del botón, 
 
+const checkComplete = () => {
+  const i = document.createElement("i")
+  i.classList.add("far","fa-check-square","icon");
+  i.addEventListener("click", compleTask);
+  return i;
+}; 
 
+
+const compleTask = (event) => {
+  const element = event.target;
+  element.classList.toggle("fas");
+  element.classList.toggle("completeIcon");
+  element.classList.toggle("far");
+};
+
+// Immediately onvoked function expression IIFE : funciones cuando se declaran funcionan
+
+})();
