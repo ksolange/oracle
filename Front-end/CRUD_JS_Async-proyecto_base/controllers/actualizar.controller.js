d16050bee186c572cd85d4938bf88e697d982bbf
Karen Alfaro <ksalfarom@gmail.com>
2023-04-19 18:02:31 +0200

    finalización CRUD

--- Front-end/CRUD_JS_Async-proyecto_base/controllers/actualizar.controller.js
+++ Front-end/CRUD_JS_Async-proyecto_base/controllers/actualizar.controller.js
@@ -10,19 +10,27 @@ const obtenerInformacion = async () => {
     
     if(id == null ){
         // console.log("hacer redirección");
-        window.location.href = './error.html'; // no sé si /screen o ./
+        window.location.href = './error.html'; // no sé si /screen o ./ + screen
     }
 
     const nombre =  document.querySelector("[data-nombre]");
 
     const email = document.querySelector("[data-email]");
 
-    const perfil = await clientServices.detalleCliente(id); /* con esto ya no hace falta el then
+    try{
+        const perfil = await clientServices.detalleCliente(id); /* con esto ya no hace falta el then
     
     .then(
         (perfil) => { */
-        nombre.value = perfil.nombre;
-        email.value =perfil.email;
+        if(perfil.nombre && perfil.email){
+            nombre.value = perfil.nombre;
+            email.value =perfil.email;
+        } else{
+            throw new Error();
+        }
+    } catch(error){
+        window.location.href = './error.html';  // no sé si /screen o ./ + screen
+    }
 };
 
 //async / await 2da clase
