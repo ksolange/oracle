23c522f99359aaacf10455e2f76760b46564e5d0
Karen Alfaro <ksalfarom@gmail.com>
2023-04-20 20:15:30 +0200

    React function components

---
+++ Front-end/React function componentes/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ClassComponent from './components/ClassComponent.jsx';
+import FuncComponent from './components/FuncComponent.jsx';
+
+function App() {
+  return (
+    <div>
+      <ClassComponent />
+      <FuncComponent />
+    </div>
+  );
+}
+
+export default App;
