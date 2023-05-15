7d47c979ff78854184ce7c34e143175d386b6b7d
Karen Alfaro <ksalfarom@gmail.com>
2023-05-15 22:14:41 +0200

    3ra clase axios y nuevo puerto 5

--- Front-end/React router-v2-codigoInicial/src/App.js
+++ Front-end/React router-v2-codigoInicial/src/App.js
@@ -2,6 +2,9 @@ import './assets/css/base/base.css';
 import './assets/css/componentes/card.css';
 import Home from "./pages/Home.jsx";
 import Sobre from "./pages/Sobre.jsx";
+import Header from './components/Header.jsx';
+
+import Page404 from './pages/Page404';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -21,9 +24,13 @@ function App() {
     <>
       {/*Router()*/}
       <Router>
+        <Header />
         <Routes>
           <Route path = '/' element = {<Home />} /> {/*1ro envío el path el camino la ruta como tal. 2. elemento q quiero q se muestre */ }
           <Route path = '/sobre' element = {<Sobre />} />
+          {/* cuando no exista el elemento buscado se puede hacer así
+          <Route path = '*' element = {<h1>No existe </h1> } /> pero no es lo mejor, lo adecuado es crear una nueva page, en la siguiente línea está el ejemplo */}
+          <Route path = '*' element = {<Page404 /> } />
         </Routes>
       </Router>
     </>
---
+++ Front-end/React router-v2-codigoInicial/src/components/ListPosts.jsx
@@ -0,0 +1,11 @@
+import "../assets/css/componentes/card.css";
+
+const ListPosts = ({ url }) => {
+    return (
+        <section className = "posts container">
+
+        </section>
+    )
+}
+
+export default ListPosts;
