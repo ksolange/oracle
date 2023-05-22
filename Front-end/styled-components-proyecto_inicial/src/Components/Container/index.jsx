ca189bc71a62fea502774efb62bf24ffb00af876
Karen Alfaro <ksalfarom@gmail.com>
2023-05-22 21:40:22 +0200

    3ra clase

--- Front-end/styled-components-proyecto_inicial/src/Components/Container/index.jsx
+++ Front-end/styled-components-proyecto_inicial/src/Components/Container/index.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 
+import styled from "styled-components";
 import Title from "../Title";
 import Account from "../Account";
 
+
+const StyledContainer = styled.div`
+  background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`
+// es cualquier etiqueta html q deseemos colocar estilo por eso div... section
+const StyledContent = styled.section` 
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
 const Container = () => {
   return (
-    <div className="container">
+    <StyledContainer>
       <Title>Smart Bank</Title>
-      <section className="content">
+      <StyledContent>
         <Account />
-      </section>
-    </div>
+      </StyledContent>
+    </StyledContainer>
   );
 };
 
--- Front-end/styled-components-proyecto_inicial/src/Components/Header/index.jsx
+++ Front-end/styled-components-proyecto_inicial/src/Components/Header/index.jsx
@@ -17,18 +17,33 @@ const Logo = styled.img`
   width: 50px;
 `
 
+const BtnHeader = styled.a`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white; 
+  color: ${ (props) => props.primary ? "white": colorPrimario };
+  background: ${(props) => props.primary ? "transparent" : "white"};
+  `
+/* ESTO VA arriba dentro de color: ${ (props) => props.primary == "activado" ? "white": colorPrimario }; // cambio de color verificando q sea el color primario en caso contrario cambia de color a colorPrimario, se puede ser más específico colocandole el "activado" y  la etiqueta BtnHeader colocar  = {"activado"}
+también se puede hacer así color: ${ (primary) => primary ? "white": colorPrimario };
+sin usar props
+*/
 
 const Header = () => {
   return (
     <StyledHeader>
       <Logo src = {logo} alt = "Logo Smart Bank" /> {/* se puede quitar el className="imagen-logo"  */}
       <div>
-        <a className="btn-secundario" href="https://google.com">
+        {/*BtnHeader primary = {"activado"} href="https://google.com"*/}
+        <BtnHeader href="https://google.com">
           Ayuda
-        </a>
-        <a className="btn-primario" href="https://google.com">
+        </BtnHeader>
+        <BtnHeader primary href="https://google.com">
           Salir
-        </a>
+        </BtnHeader>
       </div>
     </StyledHeader>
   );
