58776ff4536a509ce67b8a3fbad31a7b7f732917
Karen Alfaro <ksalfarom@gmail.com>
2023-05-23 22:00:03 +0200

    Todo ya con un nuevo archivo para styled

--- Front-end/styled-components-proyecto_inicial/src/Components/Account/index.jsx
+++ Front-end/styled-components-proyecto_inicial/src/Components/Account/index.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
-import { Icono } from "../UI/index.js";
+
+import styled from "styled-components";
+import { Icono, Box, Btn, Saldo, Detalle } from "../UI/index.js";
 import privado from "../../assets/images/privado.svg";
 import ojo from "../../assets/images/ojo.svg";
 import dinero from "../../assets/images/dinero.svg"; 
 
+
+const IconoMargin = styled(Icono)`
+   margin-top: 2px;
+`
 
 const Account = () => {
   const [toggleState, untoggle] = useState(true);
@@ -13,7 +19,7 @@ const Account = () => {
   };
 
   return (
-    <div className="box">
+    <Box>
       <h2>Cuenta</h2>
       <div style={{ fontSize: "26px", padding: "20px 0" }}>
         Saldo disponible
@@ -21,21 +27,21 @@ const Account = () => {
           <Icono src={dinero} alt="Icono de saldo" />
         </span>
         {toggleState ? (
-          <div className="saldo">
-            <span className="detalle">$</span> 8,621.50
-          </div>
+          <Saldo>
+            <Detalle >$</Detalle> 8,621.50
+          </Saldo>
         ) : null}
       </div>
 
-      <button className="btn" onClick={toggleHandler}>
+      <Btn onClick = {toggleHandler}>
         <Icono
-          style={{ marginTop: "2px" }}
+          //style={{ marginTop: "2px" }}
           // className="imagen-icono"
           src={toggleState ? privado : ojo}
           alt="Privacidad de saldo"
         />
-      </button>
-    </div>
+      </Btn>
+    </Box>
   );
 };
 
--- Front-end/styled-components-proyecto_inicial/src/Components/Container/index.jsx
+++ Front-end/styled-components-proyecto_inicial/src/Components/Container/index.jsx
@@ -3,13 +3,16 @@ import React from "react";
 import styled from "styled-components";
 import Title from "../Title";
 import Account from "../Account";
-
+import List from "../List/index.jsx";
 
 const StyledContainer = styled.div`
   background-color: #f1f1f1;
   min-height: 90vh;
   padding: 0px 15vw;
-`
+  @media (max-width: 800px) { 
+    flex-direction: column;
+  }
+`;
 // es cualquier etiqueta html q deseemos colocar estilo por eso div... section
 const StyledContent = styled.section` 
   display: flex;
@@ -23,6 +26,7 @@ const Container = () => {
       <Title>Smart Bank</Title>
       <StyledContent>
         <Account />
+        <List />
       </StyledContent>
     </StyledContainer>
   );
